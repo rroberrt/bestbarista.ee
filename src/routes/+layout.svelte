07fa1ade8062329bfcf2ebd9abb0b3951935e295
Robert Sizarov <r.sizarov@icloud.com>
2025-04-21 20:15:18 +0300

    Added content & config (#1)
    * Marked all dependencies as exact
    
    * Locked node engine version
    
    * Changed default font to Inter
    
    * Added content & config
    
    * Fixed `.gitignore` file
    
    * Deleted dist files & updated Wrangler config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,21 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex h-screen flex-col">
+	<header class="bg-black p-1">
+		<div class="flex items-center justify-center gap-x-2">
+			<img src="/us.svg" alt="UNITED STATES OF AMERICA" class="h-3" />
+			<p class="text-center text-[0.7rem] text-white">
+				Not an official website of the United States government
+			</p>
+		</div>
+	</header>
+
+	<main class="grow">
+		{@render children()}
+	</main>
+
+	<footer class="p-4">
+		<p class="text-center text-xs text-neutral-600 uppercase">Made by Bobby Shmurda</p>
+	</footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	const wallets: { ccy: string; address: string }[] = [
+		{ ccy: 'BTC', address: 'bc1qa9che390frvlkqs8md3jz9ygr3jaav60qx8fng' },
+		{ ccy: 'ETH', address: '0x8aF20cFf2f3819943CAE6669416D1C6327e66abA' },
+		{ ccy: 'SOL', address: 'Gmovjo9ixS2NGbL3E8cGcsNypCD1UvgfnA4QP4kyQMVW' },
+		{ ccy: 'LTC', address: 'ltc1qtmerhg3rvw2z6dsyh8rqvq4j5zwcg57zplp7cx' },
+		{ ccy: 'DOGE', address: 'DC9GkpG49whco53yr4s1ZPnjcKNVegAGLa' }
+	];
+</script>
+
+<section class="flex h-full">
+	<div class="container m-auto space-y-2 overflow-auto text-sm">
+		<div class="mx-auto w-fit space-y-4 sm:space-y-2">
+			{#each wallets as { ccy, address }, i (i)}
+				<div class="flex flex-col gap-x-4 font-mono sm:flex-row sm:items-center">
+					<p class="shrink-0 px-2 font-semibold sm:basis-10 sm:px-0">{ccy}</p>
+					<p class="rounded bg-neutral-100 px-2 py-1">{address}</p>
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
